1d75fcd69d91d6358334ecf759f9ccac34388f11
Maximiliano Pisso <maximiliano.pisso@gmail.com>
2021-11-15 19:08:59 -0300

    Fin desafio2

--- Desafio_2/src/paqueteDesafio2/MainClassDesafio_2.java
+++ Desafio_2/src/paqueteDesafio2/MainClassDesafio_2.java
@@ -1,9 +1,14 @@
 package paqueteDesafio2;
 
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class MainClassDesafio_2 {
 
@@ -11,7 +16,11 @@ public class MainClassDesafio_2 {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		int opcion =5;
+		
+		Scanner input = new Scanner(System.in);
+		
 		DateTimeFormatter formatTime = DateTimeFormatter.ofPattern("dd/MM/yy HH:mm:ss");
 	    System.out.println("Inicio Ejecucion: " +formatTime.format(LocalDateTime.now()));
 	    
@@ -38,15 +47,58 @@ public class MainClassDesafio_2 {
 		listPaises.add(peru);
 		listPaises.add(uruguay);
 		listPaises.add(venezuela);
+			
 		
+	   do{ 
+		   	
+		    System.out.println("INGRESE UNA OPCION DEL MENU" +"\n");
+			System.out.println("1 - BUSCAR UN PAIS POR SU CODIGO DE AREA"+"\n"+ "0 - SALIR");
+			
+			try {
+	            
+				opcion = input.nextInt(); 
+	                
+	            if (opcion != 0){
+		           
+	            	if (opcion == 1){
+		            	System.out.println("INGRESE EL CODIGO DE AREA DEL PAIS A BUSCAR ");
+		            	
+		            	
+		                int codArea = input.nextInt();  //SI ME DA UN ERROR ME LO CATCHEA EL TRY DE ARRIBA 
+            			            			            	 
+		            	List<Pais> listPaisArea = listPaises.stream()
+								.filter(pais -> pais.getCodigoArea() == codArea)
+								.collect(Collectors.toList());
+		            	
+			            	if (listPaisArea.size() == 0){
+			            		
+			            		System.out.println("NO SE ENCONTRARON REGISTROS CON EL CODIGO DE AREA INGRESADO ("+codArea+")");
+			            	}else {
+			            		System.out.println("EL PAIS CON EL CODIGO DE AREA (+"+codArea+") ES :");
+			            			for (Pais paises :listPaisArea) {
+			            				System.out.println(paises.getNombre() +" (" +paises.getId()+")");
+			            			}	
+			            	}       
+	            	}
+		            else{
+		            	System.out.println("LA OPCION INGRESADA NO ES VALIDA. INGRESE NUEVAMENE SU OPCION");
+		            }    
+		           	
+	            }else {
+	            	 System.out.println("Fin Ejecucion: " +formatTime.format(LocalDateTime.now()));
+	            }
+	        } catch (InputMismatchException ime) {
+	            System.out.println("SE INGRESO UN VALOR NO NUMERICO, VUELVA A INGRESAR LA OPCION DESEADA");
+	            input.nextLine();
+	        }
+	   }while(opcion != 0);
 		
-		 System.out.println(listPaises);
-		//listPaises.forEach();
+	   input.close();
 		
 		
 		
 
-	    System.out.println("Fin Ejecucion: " +formatTime.format(LocalDateTime.now()));	
+	    
 	}
 
 }
